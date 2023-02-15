0a699c8f9ec3d55218d63f1d66bc41435de4093d
Menion <51254101+VaguePasta@users.noreply.github.com>
2023-02-15 17:59:50 +0700

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    for (int i=0;i<=s.length()/2;i++)
+    {
+        if (s[i]!=s[s.length()-i-1])
+        {
+            cout<<"No";
+            return 0;
+        }
+    }
+    cout<<"Yes";
+}
